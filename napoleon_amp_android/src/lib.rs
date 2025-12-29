d23fc30ce9c2b3ede5c7cdc47d577c7dd51568dd
ltsoveranakin <109498476+ltsoveranakin@users.noreply.github.com>
2025-12-29 17:08:53 -0500

    Run android app (crashing with missing laaudio)

--- napoleon_amp_android/src/lib.rs
+++ napoleon_amp_android/src/lib.rs
@@ -1,5 +1,5 @@
-use eframe::egui::{CentralPanel, Context};
-use eframe::{App, Frame, NativeOptions};
+use eframe::NativeOptions;
+use napoleon_amp_client_ui::NapoleonClientApp;
 
 // #[cfg(target_os = "android")]
 #[unsafe(no_mangle)]
@@ -12,19 +12,7 @@ fn android_main(app: egui_winit::winit::platform::android::activity::AndroidApp)
     eframe::run_native(
         "Napoleon App",
         options,
-        Box::new(|cc| Ok(Box::new(BasicApp))),
+        Box::new(|cc| Ok(Box::new(NapoleonClientApp::new()))),
     )
     .unwrap();
-}
-
-struct BasicApp;
-
-impl App for BasicApp {
-    fn update(&mut self, ctx: &Context, frame: &mut Frame) {
-        CentralPanel::default().show(ctx, |ui| {
-            for i in 0..10 {
-                ui.label(format!("Hello worlda! {}", i));
-            }
-        });
-    }
 }
