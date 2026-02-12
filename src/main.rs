f8d6b9ceb9fd8176eb306ed0580acf9624a0b0bc
ltsoveranakin <109498476+ltsoveranakin@users.noreply.github.com>
2026-02-11 19:39:33 -0500

    Add window title and icon

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use eframe::egui::ViewportBuilder;
-use eframe::NativeOptions;
+use eframe::egui::{Vec2, ViewportBuilder};
+
+use eframe::{egui, NativeOptions};
+
 use napoleon_amp_client_ui::NapoleonClientApp;
 
 fn main() {
     let options = NativeOptions {
-        viewport: ViewportBuilder::default().with_inner_size([1280.0, 720.0]),
+        viewport: ViewportBuilder {
+            inner_size: Some(Vec2::new(1280.0, 720.0)),
+            title: Some("Napoleon Amp".to_string()),
+            icon: Some(std::sync::Arc::new(egui::IconData {
+                rgba: image::load_from_memory(include_bytes!("../assets/sprites/NapoleonIcon.png"))
+                    .unwrap()
+                    .to_rgba8()
+                    .to_vec(),
+                width: 512,
+                height: 512,
+                ..Default::default()
+            })),
+            ..Default::default()
+        },
         ..Default::default()
     };
 
